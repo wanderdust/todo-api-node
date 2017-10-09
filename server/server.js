cb2a7915361c3c119920695329c79b897a0aa6b5
Pablo <pablo.lopez.santori@gmail.com>
2017-10-09 13:09:41 +0100

    Add patch/todos/:id to server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const _ = require('lodash')
+,     express = require('express')
 ,     bodyParser = require('body-parser');
 
 let {ObjectID} = require('mongodb')
@@ -54,6 +55,28 @@ app.delete('/todos/:id', (req, res) => {
       res.send({todo})
     }).catch(e => res.status(404).send())
 });
+
+app.patch('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) return res.status(404).send();
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null
+  };
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if(!todo) {
+      res.status(404).send();
+    };
+
+    res.send({todo})
+  }).catch(e => res.status(404).send());
+})
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`)
